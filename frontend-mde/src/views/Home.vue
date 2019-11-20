d9044e1bb3388b19382db0845847beaff2465321
Geraldo Costa <geraldocoosta@gmail.com>
2019-11-19 22:42:16 -0300

    coisas A+

--- frontend-mde/src/views/Home.vue
+++ frontend-mde/src/views/Home.vue
@@ -2,10 +2,21 @@
   <div id="home">
     <div>
       <b-row class="my-1">
-        <b-form-input
-          v-model="nomeAparelho"
-          placeholder="Nome do aparelho"
-        ></b-form-input>
+        <b-input-group class="mt-3">
+          <b-form-input
+            v-model="nomeAparelho"
+            id="nome-aparelho"
+            placeholder="Nome do aparelho"
+          ></b-form-input>
+          <b-input-group-append>
+            <b-button
+              variant="outline-success"
+              v-if="sucess"
+              @click="salvarLocalStorage"
+              >Salvar aferição</b-button
+            >
+          </b-input-group-append>
+        </b-input-group>
       </b-row>
       <b-row class="my-1" v-if="!sucess">
         <b-form-input
@@ -21,10 +32,16 @@
     <grafico-consumo-energia
       v-if="sucess"
       :consumo="consumo"
-      :aparecerParar="timer !== null"
+      :aparecerParar="!!timer"
       @parar="onPararTimer"
       @changeSucess="changeSucess"
     />
+    <hr />
+    <b-table
+      @row-clicked="onRowClicked"
+      hover
+      :items="aparelhosAferidos"
+    ></b-table>
   </div>
 </template>
 
@@ -37,16 +54,44 @@ export default {
   data() {
     return {
       consumo: [],
-      timer: null,
+      timer: 0,
       tempoVerificacao: null,
       nomeAparelho: null,
-      sucess: false
+      sucess: false,
+      aparelhosAferidos: null
     };
   },
   components: {
     GraficoConsumoEnergia
   },
   methods: {
+    onRowClicked(item) {
+      let aparelhos = JSON.parse(localStorage.getItem("aparelhos")) || {};
+      this.consumo = aparelhos[item.aparelhos];
+      console.log(this.consumo);
+      this.$bus.$emit("novaEmissao");
+      this.sucess = true;
+    },
+    salvarLocalStorage() {
+      if (this.nomeAparelho) {
+        let aparelhos = JSON.parse(localStorage.getItem("aparelhos")) || {};
+        aparelhos[this.nomeAparelho] = this.consumo;
+        localStorage.setItem("aparelhos", JSON.stringify(aparelhos));
+        this.resetConsumo();
+      } else {
+        document.querySelector("#nome-aparelho").classList.add("invalid");
+        setTimeout(() => {
+          document.querySelector("#nome-aparelho").classList.remove("invalid");
+        }, 5000);
+      }
+    },
+    resetConsumo() {
+      this.consumo = [];
+      this.timer = null;
+      this.sucess = false;
+      this.nomeAparelho = "";
+      this.tempoVerificacao = null;
+    },
     onPararTimer() {
       if (this.timer) {
         clearTimeout(this.timer);
@@ -72,17 +117,28 @@ export default {
     buscaInformacoesCadaSegundo() {
       let tempoRequisicao = this.tempoVerificacao * 1000 || 1000;
       this.buscaInformacoes();
-      this.timer = setTimeout(this.buscaInformacoesCadaSegundo, tempoRequisicao);
+      this.timer = setTimeout(
+        this.buscaInformacoesCadaSegundo,
+        tempoRequisicao
+      );
     },
     submitFile() {
       this.sucess = true;
       this.buscaInformacoesCadaSegundo();
     }
+  },
+  created() {
+    let nomeAparelhos = Object.keys(
+      JSON.parse(localStorage.getItem("aparelhos"))
+    );
+    this.aparelhosAferidos = nomeAparelhos.map(key => {
+      return { aparelhos: key };
+    });
   }
 };
 </script>
 
-<style>
+<style scoped>
 #home {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -90,5 +146,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.invalid {
+  border-color: #dc3545 !important;
 }
 </style>
